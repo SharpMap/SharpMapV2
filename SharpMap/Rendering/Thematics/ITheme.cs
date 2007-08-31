3ca8c9c94835bc21114d1a6c43ff3e929c8a7f5b
codekaizen <codekaizen@users.noreply.github.com>
2007-08-31 19:01:27 +0000

    Moving namespaces Deleted FeatureDataTableView`1.cs

--- SharpMap/Rendering/Thematics/ITheme.cs
+++ SharpMap/Rendering/Thematics/ITheme.cs
@@ -15,25 +15,21 @@
 // along with SharpMap; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+using SharpMap.Features;
 using SharpMap.Styles;
-using SharpMap.Data;
 
 namespace SharpMap.Rendering.Thematics
 {
-	/// <summary>
-	/// Interface for rendering a thematic layer
-	/// </summary>
-	public interface ITheme
-	{
-		/// <summary>
-		/// Returns the style based on a feature.
-		/// </summary>
-		/// <param name="attribute">Attribute to calculate color from.</param>
-		/// <returns>The theme-computed style.</returns>
-		IStyle GetStyle(FeatureDataRow attribute);
-	}
+    /// <summary>
+    /// Interface for rendering a thematic layer
+    /// </summary>
+    public interface ITheme
+    {
+        /// <summary>
+        /// Returns the style based on a feature.
+        /// </summary>
+        /// <param name="attribute">Attribute to calculate color from.</param>
+        /// <returns>The theme-computed style.</returns>
+        IStyle GetStyle(FeatureDataRow attribute);
+    }
 }
