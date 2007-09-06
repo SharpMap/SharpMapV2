954dfc7cbd10c5089d768aa2261df9dacd6f6ce7
justsome.handle <justsome.handle@gmail.com>
2007-09-06 23:10:46 +0000

    Added cache test

--- SharpMap.Tests/Layers/LayerTests.cs
+++ SharpMap.Tests/Layers/LayerTests.cs
@@ -69,5 +69,53 @@ namespace SharpMap.Tests.Layers
                 Assert.Contains(row["FeatureName"], featureNames, "Unexpected visible feature");
             }
         }
+
+
+        [Test]
+        [Ignore("Test deferred until caching detection improved")]
+        public void LayerCachingTest()
+        {
+            VectorLayer layer1 = DataSourceHelper.CreateFeatureVectorLayer();
+            VectorLayer layer2 = DataSourceHelper.CreateFeatureVectorLayer();
+
+            BoundingBox box1 = new BoundingBox(0, 0, 10, 10);
+            BoundingBox box2 = new BoundingBox(35, 25, 45, 35);
+
+            layer1.AsyncQuery = false;
+            layer1.VisibleRegion = box1;
+
+            layer2.AsyncQuery = false;
+            layer2.VisibleRegion = box2;
+
+            Assert.IsTrue(layer1.Features.Envelope.Contains(box2), "Test validity check");
+            // features intersecting box1 must have extents that contain box2
+            Assert.IsFalse(dataViewEquivalent(layer1.VisibleFeatures, layer2.VisibleFeatures), "Different regions, same return set");
+
+            layer1.VisibleRegion = box2;
+            Assert.IsTrue(dataViewEquivalent(layer1.VisibleFeatures, layer2.VisibleFeatures), "Same region, different return sets");
+        }
+
+        static bool dataViewEquivalent(FeatureDataView view1, FeatureDataView view2)
+        {
+            if (view1.Count != view2.Count)
+                return false;
+
+            foreach (FeatureDataRow row1 in ((IEnumerable<FeatureDataRow>)view1))
+            {
+                bool notFound = true;
+                foreach (FeatureDataRow row2 in ((IEnumerable<FeatureDataRow>)view2))
+                {
+                    if (row1["FeatureName"] == row2["FeatureName"])
+                    {
+                        notFound = false;
+                        if (!row1.Geometry.Equals(row2.Geometry))
+                            return false;
+                    }
+                }
+                if (notFound)
+                    return false;
+            }
+            return true;
+        }
     }
 }
