c0fd4f1adfb04231e7439a0d83b9d5073292250e
codekaizen <codekaizen@users.noreply.github.com>
2007-09-11 22:16:40 +0000

    Adding tests

--- SharpMap.Tests/Features/FeatureDataViewTests.cs
+++ SharpMap.Tests/Features/FeatureDataViewTests.cs
@@ -143,7 +143,38 @@ namespace SharpMap.Tests.Features
         }
 
         [Test]
-        public void AddingRowsToTableTriggersViewNotification()
+        public void ExecutingQueryOnTableTriggersViewListChangedResetNotification()
+        {
+            FeatureProvider data = DataSourceHelper.CreateFeatureDatasource();
+            FeatureDataTable table = new FeatureDataTable();
+            BoundingBox dataExtents = data.GetExtents();
+            BoundingBox halfBounds = new BoundingBox(dataExtents.Left, dataExtents.Bottom,
+                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2);
+
+            data.ExecuteIntersectionQuery(halfBounds, table);
+            FeatureDataView view = new FeatureDataView(table);
+
+            bool addNotificationOccured = false;
+
+            view.ListChanged += delegate(object sender, ListChangedEventArgs e)
+            {
+                if (e.ListChangedType == ListChangedType.Reset)
+                {
+                    addNotificationOccured = true;
+                }
+            };
+
+            BoundingBox otherHalfBounds = new BoundingBox(
+                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2,
+                dataExtents.Right, dataExtents.Top);
+
+            data.ExecuteIntersectionQuery(otherHalfBounds, table);
+
+            Assert.IsTrue(addNotificationOccured);
+        }
+
+        [Test]
+        public void AddingRowToTableTriggersViewListChangedItemAddedNotification()
         {
             FeatureProvider data = DataSourceHelper.CreateFeatureDatasource();
             FeatureDataTable table = new FeatureDataTable();
@@ -164,11 +195,93 @@ namespace SharpMap.Tests.Features
                 }
             };
 
-            BoundingBox otherHalfBounds = new BoundingBox(
-                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2,
-                dataExtents.Right, dataExtents.Top);
+            FeatureDataRow row = table.NewRow();
+            row["Oid"] = Guid.NewGuid();
+            row["FeatureName"] = "New row";
+            row.Geometry = new Point(44, 44);
+            table.AddRow(row);
 
-            data.ExecuteIntersectionQuery(otherHalfBounds, table);
+            Assert.IsTrue(addNotificationOccured);
+        }
+
+        [Test]
+        public void RemovingRowFromTableTriggersViewListChangedItemDeletedNotification()
+        {
+            FeatureProvider data = DataSourceHelper.CreateFeatureDatasource();
+            FeatureDataTable table = new FeatureDataTable();
+            BoundingBox dataExtents = data.GetExtents();
+            BoundingBox halfBounds = new BoundingBox(dataExtents.Left, dataExtents.Bottom,
+                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2);
+
+            data.ExecuteIntersectionQuery(halfBounds, table);
+            FeatureDataView view = new FeatureDataView(table);
+
+            bool addNotificationOccured = false;
+
+            view.ListChanged += delegate(object sender, ListChangedEventArgs e)
+            {
+                if (e.ListChangedType == ListChangedType.ItemDeleted)
+                {
+                    addNotificationOccured = true;
+                }
+            };
+
+            FeatureDataRow row = table[0];
+            table.RemoveRow(row);
+
+            Assert.IsTrue(addNotificationOccured);
+        }
+
+        [Test]
+        public void ChangingRowTriggersViewListChangedItemChangedNotification()
+        {
+            FeatureProvider data = DataSourceHelper.CreateFeatureDatasource();
+            FeatureDataTable table = new FeatureDataTable();
+            BoundingBox dataExtents = data.GetExtents();
+            BoundingBox halfBounds = new BoundingBox(dataExtents.Left, dataExtents.Bottom,
+                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2);
+
+            data.ExecuteIntersectionQuery(halfBounds, table);
+            FeatureDataView view = new FeatureDataView(table);
+
+            bool addNotificationOccured = false;
+
+            view.ListChanged += delegate(object sender, ListChangedEventArgs e)
+            {
+                if (e.ListChangedType == ListChangedType.ItemChanged)
+                {
+                    addNotificationOccured = true;
+                }
+            };
+
+            FeatureDataRow row = table[0];
+            row["FeatureName"] = "Updated name";
+
+            Assert.IsTrue(addNotificationOccured);
+        }
+
+        [Test]
+        [Ignore("Not sure how to generate this notification yet.")]
+        public void MovingRowTriggersViewListChangedItemMovedNotification()
+        {
+            FeatureProvider data = DataSourceHelper.CreateFeatureDatasource();
+            FeatureDataTable table = new FeatureDataTable();
+            BoundingBox dataExtents = data.GetExtents();
+            BoundingBox halfBounds = new BoundingBox(dataExtents.Left, dataExtents.Bottom,
+                dataExtents.Left + dataExtents.Width / 2, dataExtents.Bottom + dataExtents.Height / 2);
+
+            data.ExecuteIntersectionQuery(halfBounds, table);
+            FeatureDataView view = new FeatureDataView(table);
+
+            bool addNotificationOccured = false;
+
+            view.ListChanged += delegate(object sender, ListChangedEventArgs e)
+            {
+                if (e.ListChangedType == ListChangedType.ItemMoved)
+                {
+                    addNotificationOccured = true;
+                }
+            };
 
             Assert.IsTrue(addNotificationOccured);
         }
