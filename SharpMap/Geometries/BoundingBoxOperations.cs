f284cfe972bda3b4b6aa9656780dce737a7e776e
codekaizen <codekaizen@users.noreply.github.com>
2007-09-26 02:46:45 +0000

    Adding missing BoundingBoxOperations file

---
+++ SharpMap/Geometries/BoundingBoxOperations.cs
@@ -0,0 +1,44 @@
+// Copyright 2006, 2007 - Rory Plaire (codekaizen@gmail.com)
+//
+// This file is part of SharpMap.
+// SharpMap is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// SharpMap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with SharpMap; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
+using System.Collections.Generic;
+
+namespace SharpMap.Geometries
+{
+    internal static class BoundingBoxOperations
+    {
+        internal static Geometry Difference(BoundingBox lhs, BoundingBox rhs)
+        {
+            List<BoundingBox> components = new List<BoundingBox>();
+            components.AddRange(lhs.Split(rhs.LowerLeft));
+            components.AddRange(lhs.Split(rhs.LowerRight));
+            components.AddRange(lhs.Split(rhs.UpperLeft));
+            components.AddRange(lhs.Split(rhs.UpperRight));
+
+            for (int i = components.Count - 1; i >= 0; i--)
+            {
+                if (components[i].IsEmpty || components[i].Within(rhs))
+                {
+                    components.RemoveAt(i);
+                }
+            }
+
+            BoundingBox diff = BoundingBox.Join(components.ToArray());
+            return diff.ToGeometry();
+        }
+    }
+}
