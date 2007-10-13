4884ec06fac9e37a197ecba9c1f275a078e9f65b
codekaizen <codekaizen@users.noreply.github.com>
2007-10-13 07:05:58 +0000

    Adding TestConstants.cs

---
+++ SharpMap.Tests/TestConstants.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMap.Tests
+{
+    public static class TestConstants
+    {
+        public static readonly double Epsilon = 0.0005;
+    }
+}
