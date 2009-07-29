2d48b1c513de50545ca7e0905f4de9e9583e6869
John Diss <jaundice@users.noreply.github.com>
2009-07-29 16:17:56 +0000

    SharpLayers: adding ScriptCombine attribute

--- SharpMap.Presentation.Web.SharpLayers/Properties/AssemblyInfo.cs
+++ SharpMap.Presentation.Web.SharpLayers/Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using AjaxControlToolkit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
+[assembly: ScriptCombine]
 [assembly: AssemblyTitle("SharpLayers")]
 [assembly: AssemblyDescription("Asp.Net Wrapper for OpenLayers")]
 [assembly: AssemblyConfiguration("")]
