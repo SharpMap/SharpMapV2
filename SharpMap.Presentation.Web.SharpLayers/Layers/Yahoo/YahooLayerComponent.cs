931e97acd012ee74399889188d1991de087a5a03
John Diss <jaundice@users.noreply.github.com>
2010-01-28 16:18:12 +0000

    jd: SharpLayers OL2.8tag added Yahoo layer

---
+++ SharpMap.Presentation.Web.SharpLayers/Layers/Yahoo/YahooLayerComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+
+[assembly: WebResource("SharpMap.Presentation.Web.SharpLayers.Layers.Yahoo.YahooLayerComponent.js", "text/javascript")]
+
+namespace SharpMap.Presentation.Web.SharpLayers.Layers.Yahoo
+{
+    [ClientScriptResource("SharpMap.Presentation.Web.SharpLayers.Layers.Yahoo.YahooLayerComponent",
+        "SharpMap.Presentation.Web.SharpLayers.Layers.Yahoo.YahooLayerComponent.js")]
+    [TargetControlType(typeof (Control))]
+    public class YahooLayerComponent : LayerComponent<YahooLayerBuilderParams>
+    {
+        protected override string ScriptComponentName
+        {
+            get { return "SharpMap.Presentation.Web.SharpLayers.Layers.Yahoo.YahooLayerComponent"; }
+        }
+    }
+}
