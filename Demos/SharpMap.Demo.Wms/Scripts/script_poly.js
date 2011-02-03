8ce333ba5398ef0cf4cf6ee279e1782eb59ef6bc
Diego Guidi <diegoguidi@gmail.com>
2011-02-03 15:21:30 +0000

    minor changes

--- Demos/SharpMap.Demo.Wms/Scripts/script_poly.js
+++ Demos/SharpMap.Demo.Wms/Scripts/script_poly.js
@@ -3,14 +3,13 @@ $(document).ready(function() {
     var po = org.polymaps;
         
     var mercator = new GlobalMercator();
-    var layerUrl = function(name, data) {
+    var layerUrl = function(url, layers, data) {
         var bounds = mercator.TileLatLonBounds(data.column, data.row, data.zoom)
-        log(bounds);
-        return ['/Wms.ashx',
+        return [url,
             '?HEIGHT=256&WIDTH=256&STYLES=&',
             'CRS=EPSG%3A4326&FORMAT=image%2Fpng&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&',
             'EXCEPTIONS=application%2Fvnd.ogc.se_inimage&transparent=true&',
-            'LAYERS=', name, '&',
+            'LAYERS=', layers, '&',
             'BBOX=', bounds[1], ',', -bounds[2], ',', bounds[3], ',', -bounds[0]
         ].join('');
     };
@@ -27,8 +26,11 @@ $(document).ready(function() {
         po.url(["http://{S}tile.cloudmade.com", "/1a1b06b230af4efdbb989ea99e9841af", "/998/256/{Z}/{X}/{Y}.png"].join(''))
         .hosts(["a.", "b.", "c.", ""])));
     map.add(po.image().url(function(data) {
-        return layerUrl(['poly_landmarks', 'tiger_roads', 'poi'].join(), data);
+        return layerUrl('/wms.ashx', ['poly_landmarks', 'tiger_roads', 'poi'].join(), data);
     }));
+    map.add(po.image().url(function(data) {
+        return layerUrl('http://localhost:8080/geoserver/wms', ['poly_landmarks', 'tiger_roads', 'poi'].join(), data);
+    }));    
     map.add(po.grid());
     map.add(po.compass()/*.pan("none")*/);
 });
