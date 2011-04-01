09dedbc4ffbd2fd2e7d7a5007390907f9fb99781
Diego Guidi <diegoguidi@gmail.com>
2011-04-01 14:45:49 +0000

    minor changes

--- SharpMap/Utilities/SridUtility/SridMapStrategy.cs
+++ SharpMap/Utilities/SridUtility/SridMapStrategy.cs
@@ -304,13 +304,14 @@ namespace SharpMap.Utilities.SridUtility
             List<ICoordinateSystem> lst = new List<ICoordinateSystem>();
             foreach (Proj4Reader.Proj4SpatialRefSys sys in Proj4Reader.GetSRIDs())
             {
-                ICoordinateSystem cs = fact.CreateFromWkt(sys.SrText);
+                ICoordinateSystem cs = null;
                 if (sys.Srid == 900913 || sys.Srid == 3857)
                 {
-                    const string wkt = "PROJCS[\"WGS84 / Google Mercator\", GEOGCS[\"WGS 84\", DATUM[\"World Geodetic System 1984\", SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Longitude\", EAST], AXIS[\"Latitude\", NORTH], AUTHORITY[\"EPSG\",\"4326\"]], PROJECTION[\"Mercator_1SP\"], PARAMETER[\"semi_minor\", 6378137.0], PARAMETER[\"latitude_of_origin\", 0.0], PARAMETER[\"central_meridian\", 0.0], PARAMETER[\"scale_factor\", 1.0], PARAMETER[\"false_easting\", 0.0], PARAMETER[\"false_northing\", 0.0], UNIT[\"m\", 1.0], AXIS[\"x\", EAST], AXIS[\"y\", NORTH], AUTHORITY[\"EPSG\",\"900913\"]]";                    
-                    ICoordinateSystem custom = fact.CreateFromWkt(wkt);
-                    cs = custom;
-                }                                
+                    // use wkt definition from geoserver
+                    const string wkt = "PROJCS[\"WGS84 / Google Mercator\", GEOGCS[\"WGS 84\", DATUM[\"World Geodetic System 1984\", SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Longitude\", EAST], AXIS[\"Latitude\", NORTH], AUTHORITY[\"EPSG\",\"4326\"]], PROJECTION[\"Mercator_1SP\"], PARAMETER[\"semi_minor\", 6378137.0], PARAMETER[\"latitude_of_origin\", 0.0], PARAMETER[\"central_meridian\", 0.0], PARAMETER[\"scale_factor\", 1.0], PARAMETER[\"false_easting\", 0.0], PARAMETER[\"false_northing\", 0.0], UNIT[\"m\", 1.0], AXIS[\"x\", EAST], AXIS[\"y\", NORTH], AUTHORITY[\"EPSG\",\"900913\"]]";
+                    cs = fact.CreateFromWkt(wkt);                    
+                }
+                else fact.CreateFromWkt(sys.SrText);
                 lst.Add(cs);
             }
             return lst;
