4675fecc58f4fec812e11da963f2fab4dd75ba16
Felix Obermaier <FObermaier@users.noreply.github.com>
2012-11-26 09:56:57 +0000

    [branches/initalgdalintegration]

--- SharpMap/Diagnostics/EnumerablePrinter.cs
+++ SharpMap/Diagnostics/EnumerablePrinter.cs
@@ -1,25 +1,84 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
+using GeoAPI.SystemCoreReplacement;
 
 namespace SharpMap.Diagnostics
 {
-    class EnumerablePrinter
+    /// <summary>
+    /// Utility class to print a series of items
+    /// </summary>
+    public static class EnumerablePrinter
     {
+        /// <summary>
+        /// Prints a series of <paramref name="enumerable">items</paramref> using the <see cref="object.ToString()"/> method
+        /// </summary>
+        /// <param name="enumerable">The items</param>
+        /// <returns>A <see cref="string"/></returns>
         public static String Print(IEnumerable enumerable)
         {
-            StringBuilder buffer = new StringBuilder();
+            return Print(enumerable, o => o.ToString());
+        }
 
-            foreach (Object value in enumerable)
+        /// <summary>
+        /// Prints a series of <paramref name="enumerable">items</paramref> using the <paramref name="decorator"/> method
+        /// </summary>
+        /// <param name="enumerable">The items</param>
+        /// <param name="decorator">The function to convert from <see cref="object"/> to <see cref="string"/></param>
+        /// <returns>A <see cref="string"/></returns>
+        public static String Print(IEnumerable enumerable, Func<object, string> decorator)
+        {
+            var buffer = new StringBuilder();
+
+            foreach (var value in enumerable)
             {
-                buffer.Append(value);
-                buffer.Append(',');
-                buffer.Append(' ');
+                buffer.Append(decorator(value));
+                buffer.Append(", ");
             }
 
             if (buffer.Length >= 2)
             {
-                buffer.Length -= 2;   
+                buffer.Length -= 2;
+            }
+
+            return buffer.ToString();
+        }
+
+        /// <summary>
+        /// Prints a series of <paramref name="enumerable"><typeparamref name="T"/> items</paramref> using the <see cref="object.ToString()"/> method
+        /// </summary>
+        /// <param name="enumerable">The items</param>
+        /// <returns>A string</returns>
+        public static String Print<T>(IEnumerable<T> enumerable)
+        {
+            return Print(enumerable, o => o.ToString());
+        }
+
+        /// <summary>
+        /// Prints a series of <paramref name="enumerable">items</paramref> using the <paramref name="decorator"/> method
+        /// </summary>
+        /// <param name="enumerable">The items</param>
+        /// <param name="decorator">The function to convert from <typeparamref name="T"/> to <see cref="string"/></param>
+        /// <returns>A <see cref="string"/></returns>
+        public static String Print<T>(IEnumerable<T> enumerable, Func<T, string> decorator)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException("enumerable");
+            if (decorator == null)
+                throw new ArgumentNullException("decorator");
+
+            var buffer = new StringBuilder();
+
+            foreach (var value in enumerable)
+            {
+                buffer.Append(decorator(value));
+                buffer.Append(", ");
+            }
+
+            if (buffer.Length >= 2)
+            {
+                buffer.Length -= 2;
             }
 
             return buffer.ToString();
--- SharpMap/Diagnostics/TraceCategories.cs
+++ SharpMap/Diagnostics/TraceCategories.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿
+using System;
 
 namespace SharpMap.Diagnostics
 {
+    /// <summary>
+    /// Collection of trace categories
+    /// </summary>
     public static class TraceCategories
     {
+        /// <summary>
+        /// Label for trace messages related to presentation
+        /// </summary>
         public static readonly String Presentation = "Presentation";
+        
+        /// <summary>
+        /// Label for trace messages related to rendering
+        /// </summary>
         public static readonly String Rendering = "Rendering";
+        
+        /// <summary>
+        /// Label for trace messages related to data access
+        /// </summary>
         public static readonly String DataAccess = "DataAccess";
+
+        /// <summary>
+        /// Label for trace messages related to tool usage
+        /// </summary>
         public static readonly String Tool = "Tool";
     }
 }
